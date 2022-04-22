66522864de0583ce911775a3ffd34fc939bb7a37
JohnNadja <cv3802@innovaccion.mx>
2022-04-22 18:23:09 -0500

    Solving UserService test 1

--- Twitter/test/services/UserService.test.js
+++ Twitter/test/services/UserService.test.js
@@ -2,10 +2,10 @@ const UserService = require('./../../app/services/UserService')
 
 describe('Test for User Service', () => {
     test('1. Create a new user using the UserService', () => {
-        const user = new UserService(1, "johnnadja", "John")
-        expect(user.username).toBe("johnnadja");
-        expect(user.name).toBe("John");
-        expect(user.id).toBe(1);
-        expect(user.bio).not.toBeUndefinied();
+        const user = UserService.create(1, "john", "John")
+        expect(user.username).toBe("john")
+        expect(user.name).toBe("John")
+        expect(user.id).toBe(1)
+        expect(user.bio).not.toBeUndefined()
     });
 });
