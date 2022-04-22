f663bf5e8e1d0c24f7c176a39e6c5a794f8fcfea
JohnNadja <cv3802@innovaccion.mx>
2022-04-22 12:58:12 -0500

    Adding Requeriment 2 on test = Atributes

--- Twitter/test/Models/User.test.js
+++ Twitter/test/Models/User.test.js
@@ -2,8 +2,18 @@ const User = require('./../../app/Models/User')
 
 describe("Test para User", () =>{
     test("Creación de User", () =>{
-        const user = new User (1, "JohnNadja", "John", "Bio", "dateCreated", "lastUpdated")
+        const user = new User (1, "JohnNadja", "John", "Bio")
         expect(user.id).toBe(1)
         expect(user.username).toBe("JohnNadja")
+        expect(user.name).toBe("John")
+        expect(user.bio).toBe("Bio")
+        // expect(user.dateCreated).toBe("dateCreated")
+        // expect(user.lastUpdated).toBe("lastUpdated")
+    })
+
+    test("Requerimiento 2: Fechas en atributos de User", () =>{
+        const user = new User (1, "JohnNadja", "john", "Bio")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.dateCreated).not.toBeUndefined()
     })
 })
