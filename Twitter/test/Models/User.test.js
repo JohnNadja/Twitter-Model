6cf980b71fdf4f496874905242ca99623bd87a95
JohnNadja <cv3802@innovaccion.mx>
2022-04-22 12:28:45 -0500

    Creating class User and respective test

--- Twitter/test/Models/User.test.js
+++ Twitter/test/Models/User.test.js
@@ -1,8 +1,9 @@
-const User = require('./../Models/User')
+const User = require('./../../app/Models/User')
 
-describe("Prueba de jest", () =>{
-    test("Prueba 1", () =>{
+describe("Test para User", () =>{
+    test("Creación de User", () =>{
         const user = new User (1, "JohnNadja", "John", "Bio", "dateCreated", "lastUpdated")
         expect(user.id).toBe(1)
+        expect(user.username).toBe("JohnNadja")
     })
 })
